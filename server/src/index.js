8258ff78a7a1e812d9ffba590f4b57fdcabb9a75
Fahmi Dinsefa <116361774+poricf@users.noreply.github.com>
2025-06-19 02:28:04 +0900

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -14,6 +14,7 @@ import utilityRoutes from './routes/utilityRoutes.js';
 import notFound from './utils/notFound.js';
 import errorHandler from './utils/errorHandler.js';
 
+
 dotenv.config();
 
 const app = express();
